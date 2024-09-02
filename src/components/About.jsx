e024a44bd60390be4a5661be5b4926173d3092a9
farrukh.khan <farrukh.khan@sybrid.com>
2024-09-02 20:56:50 +0500

    Till Feedback

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
@@ -21,7 +22,7 @@ const ServiceCard = ({ index, title, icon }) => {
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
         >
           <img src={icon} alt={title} className="w-16 h-16 object-contain" />
-          <h3 className="text-white text-[20px]">
+          <h3 className="text-white text-[20px] font-bold text-center">
             {title}
           </h3>
         </div>
@@ -52,4 +53,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
